662a13f325a45c58d8b6dc9860da9faaae1bc078
vincensiusadyatma <adyatmav@gmail.com>
2024-11-10 09:31:06 +0700

    feat : create handler for edit book

--- src/handler.js
+++ src/handler.js
@@ -53,4 +53,97 @@ const addBookHandler = function(request,h){
 
 };
 
-module.exports = {addBookHandler};
+const getAllBookHandler = function(request,h){
+    const response = h.response(
+        {
+            status: "success",
+            message : "Buku berhasil didapatkan",
+            data : {
+                books
+            }
+        }
+    )
+
+    return response;
+};
+
+
+const getBookByIdHandler = function(request,h){
+    const {bookId} = request.params;
+
+    const book = books.filter(function(book){
+        return book.id == bookId;
+    })[0];
+
+    if (book) {
+        return h.response({
+            status: "success",
+            data : {
+                book
+            }
+        }).code(200);
+    }else{
+        return h.response({
+             "status": "fail",
+             "message": "Buku tidak ditemukan"
+        }).code(400);
+    }
+};
+
+const editBookByIdHandler = function(request,h){
+    const {bookId} = request.params;
+    const {name, year, author, summary, publisher, pageCount, readPage, reading} = request.payload;
+    const updatedAt = new Date().toISOString();
+
+    const index = books.findIndex(function(book){
+        return book.id == bookId
+    })
+
+    if (name == null || name == undefined) {
+        const response = h.response({
+           "status": "fail",
+            "message": "Gagal memperbarui buku. Mohon isi nama buku"
+       })
+       response.code(400)
+       return response
+
+    }else if(readPage > pageCount){
+        const response = h.response({
+            "status": "fail",
+            "message": "Gagal memperbarui buku. readPage tidak boleh lebih besar dari pageCount"
+        })
+        response.code(400)
+        return response
+    }else if(index == -1){
+        const response = h.response({
+            "status": "fail",
+            "message": "Gagal memperbarui buku. Id tidak ditemukan"
+        })
+        response.code(404)
+        return response
+    }else if(index !== -1){
+        books[index] = {
+            ...books[index],
+            name,
+            year,
+            author,
+            summary,
+            publisher,
+            pageCount,
+            readPage,
+            reading,
+            updatedAt
+        }
+        const response = h.response({
+             "status": "success",
+             "message": "Buku berhasil diperbarui"
+        })
+
+        response.code(200)
+        return response;
+    }
+
+  
+}
+
+module.exports = {addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,27 @@
-const {addBookHandler} = require('./handler');
+const {addBookHandler, getAllBookHandler, getBookByIdHandler, editBookByIdHandler} = require('./handler');
 const routes = [
     
     {
         method : 'POST',
         path : '/books',
         handler : addBookHandler
-    }   
+    },
+    {
+        method : 'GET',
+        path : '/books',
+        handler : getAllBookHandler
+    },
+    {
+        method : 'GET',
+        path : '/books/{bookId}',
+        handler : getBookByIdHandler
+    },
+    {
+        method : 'PUT',
+        path : '/books/{bookId}',
+        handler : editBookByIdHandler
+    } 
+    
 ]
 
 module.exports = routes;
